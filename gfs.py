d0fab1f28d95586aea08686ca8341e565dfb323f
IlluminatiFish <45714340+IlluminatiFish@users.noreply.github.com>
2022-03-12 19:47:00 +0000

    Update gfs.py
    - Formatted script
    - Added tabluar script output

--- gfs.py
+++ gfs.py
@@ -10,6 +10,7 @@ from enum import Enum
 from rich import print
 from queue import Queue
 from threading import Thread
+from tabulate import tabulate
 from bs4 import BeautifulSoup
 from typing import Dict, List
 
@@ -18,6 +19,7 @@ class FieldType(Enum):
     """
     An enum to define all possible field types
     """
+
     SHORT_TEXT = 0
     LONG_TEXT = 1
     MULTIPLE_CHOICE = 2
@@ -33,6 +35,7 @@ class Field(object):
     """
     A Field object used to define fields
     """
+
     validation: bool
     required: bool
     has_choices: bool
@@ -48,6 +51,7 @@ class Choice(object):
     """
     A Choice object used to define choices
     """
+
     def __init__(self):
         self.choice_name = None
 
@@ -56,6 +60,7 @@ class GoogleFormSpammerException(Exception):
     """
     A class to be raised when errors occur
     """
+
     pass
 
 
@@ -76,10 +81,12 @@ class GoogleFormSpammer:
                 GoogleFormSpammerException: If `form_url` is None or if it is not a valid form url
         """
         if form_url is None:
-            raise GoogleFormSpammerException('form_url cannot be None')
+            raise GoogleFormSpammerException("form_url cannot be None")
 
-        if not re.match('https://docs.google.com/forms/d/e/[A-Za-z0-9_-]{56}/formResponse', form_url):
-            raise GoogleFormSpammerException('form_url is not valid')
+        if not re.match(
+            "https://docs.google.com/forms/d/e/[A-Za-z0-9_-]{56}/formResponse", form_url
+        ):
+            raise GoogleFormSpammerException("form_url is not valid")
 
         self.form_url = form_url
         self.required_only = required_only
@@ -94,20 +101,25 @@ class GoogleFormSpammer:
 
         """
         response = requests.get(self.form_url)
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, "html.parser")
 
-        divs = soup.find_all('div')
+        divs = soup.find_all("div")
 
-        replacements = {'%.@.': '[', 'null': '"null"', 'true': '"true"', 'false': '"false"'}
+        replacements = {
+            "%.@.": "[",
+            "null": '"null"',
+            "true": '"true"',
+            "false": '"false"',
+        }
 
         fields = []
 
         for div in divs:
 
             # Find all div tags with the attribute `jsmodel`
-            if 'jsmodel' in div.attrs.keys():
+            if "jsmodel" in div.attrs.keys():
 
-                data_params = div.attrs.get('data-params')
+                data_params = div.attrs.get("data-params")
 
                 # Fix array so it can be handled by Python
                 for old, new in replacements.items():
@@ -127,7 +139,7 @@ class GoogleFormSpammer:
                 field.id = response_data[0][0]
 
                 field.validation = len(response_data[0][4]) > 0
-                field.required = True if response_data[0][2] == 'true' else False
+                field.required = True if response_data[0][2] == "true" else False
                 field.has_choices = False
 
                 if len(response_data[0][1]) > 0:
@@ -171,28 +183,35 @@ class GoogleFormSpammer:
 
             # To support the date and time fields we must make a specific case for each
             if field.type == FieldType.TIME:
-                post_data[f'entry.{field.id}_hour'] = f'{random.randint(0, 23):02d}'
-                post_data[f'entry.{field.id}_minute'] = f'{random.randint(0, 59):02d}'
+                post_data[f"entry.{field.id}_hour"] = f"{random.randint(0, 23):02d}"
+                post_data[f"entry.{field.id}_minute"] = f"{random.randint(0, 59):02d}"
 
             elif field.type == FieldType.DATE:
-                post_data[f'entry.{field.id}_year'] = str(random.randint(2000, 2022))
-                post_data[f'entry.{field.id}_month'] = str(random.randint(1, 12))
-                post_data[f'entry.{field.id}_day'] = str(random.randint(1, 31))
+                post_data[f"entry.{field.id}_year"] = str(random.randint(2000, 2022))
+                post_data[f"entry.{field.id}_month"] = str(random.randint(1, 12))
+                post_data[f"entry.{field.id}_day"] = str(random.randint(1, 31))
 
             else:
 
                 # Only field that has validation is the email fields if found
                 if field.validation:
-                    email_providers = ['yahoo.com', 'hotmail.com', 'outlook.net', 'gmail.com']
-                    selected_choice = ''.join(random.choice(chars) for _ in range(data_length)) + '@' + random.choice(email_providers)
+                    email_providers = [
+                        "yahoo.com",
+                        "hotmail.com",
+                        "outlook.net",
+                        "gmail.com",
+                    ]
+                    selected_choice = "".join(random.choice(chars) for _ in range(data_length)) + "@" + random.choice(email_providers)
 
                 elif field.has_choices:
                     selected_choice = random.choice(field.choices).choice_name
 
                 else:
-                    selected_choice = ''.join(random.choice(chars) for _ in range(data_length))
+                    selected_choice = "".join(
+                        random.choice(chars) for _ in range(data_length)
+                    )
 
-                post_data[f'entry.{field.id}'] = selected_choice
+                post_data[f"entry.{field.id}"] = selected_choice
 
         return post_data
 
@@ -204,7 +223,7 @@ class GoogleFormSpammer:
                 response.status_code (int): An integer stating the HTTP status code of the response
 
         """
-        response = requests.post(self.form_url, params = self.generate_post_data())
+        response = requests.post(self.form_url, params=self.generate_post_data())
         return response.status_code
 
     def threader(self) -> None:
@@ -217,32 +236,34 @@ class GoogleFormSpammer:
             queue.task_done()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='A script to spam Google Forms with garbage data')
+    parser = argparse.ArgumentParser(description="A script to spam Google Forms with garbage data")
 
-    parser.add_argument('-u', '--url', type = str, required = True, help = 'The url of the google form')
-    parser.add_argument('-o', '--required', type = bool, default = False, help = 'If you only want to fill in the required fields')
-    parser.add_argument('-r', '--requests', type=int, default=500, help='The amount of requests to execute')
-    parser.add_argument('-t', '--threads', type=int, default=50, help='The amount of threads to use')
+    parser.add_argument("-u", "--url", type=str, required=True, help="The url of the google form")
+    parser.add_argument("-o", "--required", type=bool, default=False, help="If you only want to fill in the required fields")
+    parser.add_argument("-r", "--requests", type=int, default=500, help="The amount of requests to execute")
+    parser.add_argument("-t", "--threads", type=int, default=50, help="The amount of threads to use")
 
     args = parser.parse_args()
 
     if args.url is None:
-        print(f'[bold #F04349]Invalid argument, supply a form url[/bold #F04349]')
+        print(f"[bold #F04349][-] Invalid argument, supply a form url[/bold #F04349]")
         exit(-1)
 
-    print(
-          '\n'
-    	  f'[bold #FC970F]Starting spammer...[/bold #FC970F]\n'
-          f'[bold #EEBD31]'
-          f'URL: {args.url}\n'
-          f'Requests: {args.requests}\n'
-          f'Threads: {args.threads}\n'
-          f'Required Fields: {args.required}\n'
-          f'[/bold #EEBD31]'
-	      '\n'
+    print("")
+    print("[bold #FC970F][~] Starting spammer...[/bold #FC970F]\n")
+    parameter_table = tabulate(
+        [
+            ["URL", args.url],
+            ["Requests", args.requests],
+            ["Threads", args.threads],
+            ["Required Fields", args.required],
+        ],
+        tablefmt="pretty",
+        colalign=("center", "left"),
     )
+    print(f"[bold #F2B44B]{parameter_table}[/bold #F2B44B]\n")
 
     spammer = GoogleFormSpammer(args.url, args.required)
 
@@ -260,15 +281,16 @@ if __name__ == '__main__':
 
     queue.join()
 
-    total_time = time.perf_counter() - start
-    req_per_ns = args.requests / total_time
+    total_time = round(time.perf_counter() - start, 2)
+    req_per_sec = round(args.requests / total_time, 3)
 
-    print(
-          '\n'
-    	  '[bold #07FA1C]Script finished![/bold #07FA1C]\n'
-    	  f'[bold #31EE42]'
-    	  f'Execution time: {total_time}s\n'
-    	  f'Speed: {req_per_ns} req/s\n'
-    	  f'[/bold #31EE42]'
-    	  '\n'
+    print("[bold #07FA1C][=] Spammer finished![/bold #07FA1C]\n")
+    results_table = tabulate(
+        [
+            ["Execution Time", f"{total_time}s"],
+            ["Speed", f"{req_per_sec} req/s"]
+        ],
+        tablefmt="pretty",
+        colalign=("center", "left"),
     )
+    print(f"[bold #31EE42]{results_table}[/bold #31EE42]\n")
